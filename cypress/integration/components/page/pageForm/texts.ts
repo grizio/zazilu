c515a364dc7d9d1ed48cb0cf1489014fc5b4efef
grizio <gaetan@rizio.fr>
2021-02-24 10:04:35 +0100

    Add more cypress tests

--- cypress/integration/components/page/pageForm/texts.ts
+++ cypress/integration/components/page/pageForm/texts.ts
@@ -8,7 +8,6 @@ describe("TextEdit", () => {
   const thirdParagraph = "[data-test-id=HdVEMtWlRqytL7YHz7rt]"
   const fourthParagraph = "[data-test-id=h409p9NGTYsdQntU8lZO]"
 
-  //*
   it("should update accordingly to user writing", () => {
     initialize()
     cy.get(firstParagraph).click()
@@ -67,7 +66,6 @@ describe("TextEdit", () => {
     cy.get(fourthParagraph).type("xxx")
     cy.get(fourthParagraph).should("have.text", " Aliqxxxuam erat volutpat. Proin ornare luctus iaculis. Duis ac dolor dui. Fusce non pretium metus, et ullamcorper velit. Maecenas ornare massa in orci varius posuere. Nam sollicitudin ullamcorper pulvinar. In libero magna, pretium quis felis sit amet, aliquam interdum est. Suspendisse pretium magna vitae turpis molestie scelerisque. Curabitur vel neque a sem lacinia fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eget sapien pulvinar, vestibulum enim id, euismod ipsum. Morbi aliquam vestibulum aliquam. Suspendisse elementum ex at posuere lobortis. Nulla nisl velit, ultricies id sodales vel, bibendum quis nulla. Nulla facilisi.")
   })
-  // */
 
   it("should correctly move cursor to the top, remaining on the bloc when multiline and not on first line", () => {
     initialize()
@@ -94,5 +92,47 @@ describe("TextEdit", () => {
     cy.get(fourthParagraph).type("{uparrow}")
     cy.get(thirdParagraph).type("xxx")
     cy.get(thirdParagraph).should("have.text", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id finibus mauris. Morbi et porta nisl, vitae efficitur tellus. Etiam id cursus lectus, eu faucibus diam. Suspendisse fermentum magna eu justo elementum sagittis. Donec eget malesuada elit, nec gravida ligula. Aenean ut odio in elit pharetra volutpat. Phasellus finibus leo et ipsum vestibulum, vitae semper ligula venenatis. Mauris quam lectus, aliquam vitae sapien in, imperdiet eleifend dolor. Curabitur pharetra maximus sagittis. Donec dignissim eu nisi sed viverra. Vivamus cursus erat eu ligula auctor, in fringilla libero congue. Maecenas erat nisi, sagittis vitae mi nec, tincidunt hendrerit lectus. Nulla lobortis mollis trxxxistique. Aliquam erat volutpat.")
+  })
+
+  it("should correctly add a new paragraph", () => {
+    initialize()
+    cy.get(firstParagraph).click()
+    cy.get(firstParagraph).type("{enter}")
+    cy.get(`${firstParagraph} + p`).should("be.empty")
+    cy.get(`${firstParagraph} + p`).type("A new paragraph!")
+    cy.get(`${firstParagraph} + p`).should("have.text", "A new paragraph!")
+    cy.get(secondParagraph).should("have.text", "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
+  })
+
+  it("should correctly add a new paragraph with the text on right of cursor", () => {
+    initialize()
+    cy.get(firstParagraph).click()
+    cy.get(firstParagraph).type("{leftarrow}{leftarrow}{leftarrow}{leftarrow}{leftarrow}")
+    cy.get(firstParagraph).type("{enter}")
+    cy.get(`${firstParagraph} + p`).should("have.text", "text.")
+    cy.get(`${firstParagraph} + p`).type("Changed ")
+    cy.get(`${firstParagraph} + p`).should("have.text", "Changed text.")
+    cy.get(secondParagraph).should("have.text", "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
+  })
+
+  it("should correctly merge two paragraphs when using backspace", () => {
+    initialize()
+    cy.get(secondParagraph).click()
+    cy.get(secondParagraph).type("{movetostart}")
+    cy.get(secondParagraph).type("{backspace}")
+    cy.get(firstParagraph).should("have.text", "This is some text.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
+    cy.get(secondParagraph).should("not.exist")
+    cy.get(firstParagraph).type("xxx")
+    cy.get(firstParagraph).should("have.text", "This is some text.xxxClass aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
+  })
+
+  it("should correctly merge two paragraphs when using delete", () => {
+    initialize()
+    cy.get(firstParagraph).click()
+    cy.get(firstParagraph).type("{del}")
+    cy.get(firstParagraph).should("have.text", "This is some text.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
+    cy.get(secondParagraph).should("not.exist")
+    cy.get(firstParagraph).type("xxx")
+    cy.get(firstParagraph).should("have.text", "This is some text.xxxClass aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.")
   })
 })
