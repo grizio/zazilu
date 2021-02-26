bf16db5842672aee5f5201220c6b157e153135ec
grizio <gaetan@rizio.fr>
2021-02-26 14:35:17 +0100

    Restructure `KeyboardListener`
    We restructure `KeyboardListener` so we directly have a map of `(ActionKey, Array<KeyboardAction>)`. It will give a direct access to actions for given `ActionKey` directly instead of looping through all built actions. It should improve performances with lots of actions and mainly simplify debug.

--- src/components/bloc/helpers/KeyboardListener.ts
+++ src/components/bloc/helpers/KeyboardListener.ts
@@ -4,7 +4,7 @@ import { getCurrentCaretPosition, getCurrentSelection } from "../../../utils/dom
 type Letter =
   | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m"
   | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
-type Keys =
+type Key =
   | "ArrowDown"
   | "ArrowLeft"
   | "ArrowRight"
@@ -14,75 +14,73 @@ type Keys =
   | "Enter"
   | " "
   | Letter
-type Modifiers = "ctrl" | "alt" | "shift"
-type ActionKey =
-  | Keys
-  | `${Modifiers}+${Keys}`
-  | `${Modifiers}+${Modifiers}+${Keys}`
-  | `${Modifiers}+${Modifiers}+${Modifiers}+${Keys}`
+type ActionKey = `${"ctrl+" | ""}${"alt+" | ""}${"shift+" | ""}${Key}`
 
 type KeyboardAction = (event: KeyboardEvent) => boolean
 
 export class KeyboardListener {
-  readonly actions: Array<KeyboardAction>
+  readonly actions: Partial<Record<ActionKey, Array<KeyboardAction>>>
 
-  constructor(actions: Array<KeyboardAction> = []) {
+  constructor(actions: Partial<Record<ActionKey, Array<KeyboardAction>>> = {}) {
     this.actions = actions
   }
 
   on = (actionKey: ActionKey): ActionBuilder<{}> => {
-    return new ActionBuilder(this.buildKeyFilter(actionKey), this, () => ({}))
-  }
-
-  private buildKeyFilter = (actionKey: ActionKey): (event: KeyboardEvent) => boolean => {
-    const parts = actionKey.split("+")
-    const key = parts[parts.length - 1]
-    const ctrlModifier = parts.includes("ctrl")
-    const altModifier = parts.includes("alt")
-    const shiftModifier = parts.includes("shift")
-    return (event: KeyboardEvent) =>
-      event.key === key &&
-      event.ctrlKey === ctrlModifier &&
-      event.altKey === altModifier &&
-      event.shiftKey === shiftModifier
+    return new ActionBuilder({
+      actionKey,
+      filter: () => true,
+      extractor: () => ({}),
+      listener: this
+    })
   }
 
   onEvent = (event: KeyboardEvent): void => {
-    for (let i = 0; i < this.actions.length; i++) {
-      if (this.actions[i](event)) {
+    const actionsForKey = this.actions[this.buildActionKeyCode(event)] ?? []
+    for (let i = 0; i < actionsForKey.length; i++) {
+      if (actionsForKey[i](event)) {
         event.preventDefault()
         return
       }
     }
   }
+
+  private buildActionKeyCode = (event: KeyboardEvent): ActionKey => {
+    const ctrlPrefix = event.ctrlKey ? "ctrl+" : ""
+    const altPrefix = event.altKey ? "alt+" : ""
+    const shiftPrefix = event.shiftKey ? "shift+" : ""
+    return ctrlPrefix + altPrefix + shiftPrefix + event.key as ActionKey
+  }
 }
 
+type ActionBuilderState<Detail> = {
+  actionKey: ActionKey
+  filter: (event: KeyboardEvent, detail: Detail) => boolean
+  extractor: (event: KeyboardEvent) => Detail | undefined
+  listener: KeyboardListener
+}
 class ActionBuilder<Detail extends {}> {
-  private readonly _filter: (event: KeyboardEvent, detail: Detail) => boolean
-  private readonly _listener: KeyboardListener
-  private readonly _extractor: (event: KeyboardEvent) => Detail | undefined
+  private readonly state: ActionBuilderState<Detail>
 
-  constructor(filter: (event: KeyboardEvent, detail: Detail) => boolean, listener: KeyboardListener, extractor: (event: KeyboardEvent) => Detail | undefined) {
-    this._filter = filter
-    this._listener = listener
-    this._extractor = extractor
+  constructor(state: ActionBuilderState<Detail>) {
+    this.state = state
   }
 
   withCaret = (): ActionBuilder<Detail & { caret: Caret }> => {
     return new ActionBuilder(
-      this._filter,
-      this._listener,
-      (event) => {
-        const previousDetail = this._extractor(event)
-        if (previousDetail !== undefined) {
-          const caret = getCurrentCaretPosition()
-          if (caret !== undefined) {
-            return { ...previousDetail, caret: new Caret(caret) }
+      {
+        ...this.state,
+        extractor: (event) => {
+          const previousDetail = this.state.extractor(event)
+          if (previousDetail !== undefined) {
+            const caret = getCurrentCaretPosition()
+            if (caret !== undefined) {
+              return { ...previousDetail, caret: new Caret(caret) }
+            } else {
+              return undefined
+            }
           } else {
             return undefined
           }
-        } else {
-          return undefined
         }
       }
     )
@@ -90,39 +88,41 @@ class ActionBuilder<Detail extends {}> {
 
   withUniqueSelection = (): ActionBuilder<Detail & { selection: UniqueSelection }> => {
     return new ActionBuilder(
-      this._filter,
-      this._listener,
-      (event) => {
-        const previousDetail = this._extractor(event)
-        if (previousDetail !== undefined) {
-          const selection = getCurrentSelection()
-          if (selection !== undefined) {
-            return { ...previousDetail, selection: new UniqueSelection(selection) }
+      {
+        ...this.state,
+        extractor: (event) => {
+          const previousDetail = this.state.extractor(event)
+          if (previousDetail !== undefined) {
+            const selection = getCurrentSelection()
+            if (selection !== undefined) {
+              return { ...previousDetail, selection: new UniqueSelection(selection) }
+            } else {
+              return undefined
+            }
           } else {
             return undefined
           }
-        } else {
-          return undefined
         }
       }
     )
   }
 
   filter = (fn: (params: { event: KeyboardEvent } & Detail) => boolean): ActionBuilder<Detail> => {
-    return new ActionBuilder<Detail>(
-      (event, detail) => this._filter(event, detail) && fn({ event, ...detail }),
-      this._listener,
-      this._extractor
-    )
+    return new ActionBuilder<Detail>({
+      ...this.state,
+      filter: (event, detail) => this.state.filter(event, detail) && fn({ event, ...detail })
+    })
   }
 
   process = (fn: (params: { event: KeyboardEvent } & Detail) => void): KeyboardListener => {
-    return new KeyboardListener(
-      [
-        ...this._listener.actions,
+    const existingActionsForCode = this.state.listener.actions[this.state.actionKey] ?? []
+    const newActions = {
+      ...this.state.listener.actions,
+      [this.state.actionKey]: [
+        ...existingActionsForCode,
         (event: KeyboardEvent) => {
-          const detail = this._extractor(event)
-          if (detail !== undefined && this._filter(event, detail)) {
+          const detail = this.state.extractor(event)
+          if (detail !== undefined && this.state.filter(event, detail)) {
             fn({ event, ...detail })
             return true
           } else {
@@ -130,22 +130,26 @@ class ActionBuilder<Detail extends {}> {
           }
         }
       ]
-    )
+    }
+    return new KeyboardListener(newActions)
   }
 
   tryProcess = (fn: (params: { event: KeyboardEvent } & Detail) => boolean): KeyboardListener => {
-    return new KeyboardListener(
-      [
-        ...this._listener.actions,
+    const existingActionsForCode = this.state.listener.actions[this.state.actionKey] ?? []
+    const newActions = {
+      ...this.state.listener.actions,
+      [this.state.actionKey]: [
+        ...existingActionsForCode,
         (event: KeyboardEvent) => {
-          const detail = this._extractor(event)
-          if (detail !== undefined && this._filter(event, detail)) {
+          const detail = this.state.extractor(event)
+          if (detail !== undefined && this.state.filter(event, detail)) {
             return fn({ event, ...detail })
           } else {
             return false
           }
         }
       ]
-    )
+    }
+    return new KeyboardListener(newActions)
   }
 }
