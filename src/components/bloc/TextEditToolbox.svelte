9ddb8117c1e02b6cf5d5cc950bb3f9180ced8823
grizio <gaetan@rizio.fr>
2021-03-13 10:37:13 +0100

    Add quick styles to toolbox

--- src/components/bloc/TextEditToolbox.svelte
+++ src/components/bloc/TextEditToolbox.svelte
@@ -28,13 +28,48 @@
   .toolbox {
     position: absolute;
     bottom: 0;
+    background-color: #ffffff;
+    border: 1px solid #999999;
+    border-radius: 4px;
+    box-shadow: 0 2px 5px rgba(102, 102, 102, 0.5);
+    padding: 4px;
+  }
+
+  button {
+    background-color: #ffffff;
+    border: 1px solid #999999;
+    padding: 4px 8px;
+    transition: all ease-in-out 250ms;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #cccccc;
+  }
+
+  .bold {
+    font-weight: 700;
+  }
+
+  .italic {
+    font-style: italic;
+  }
+
+  .link {
+    text-decoration: underline;
   }
 </style>
 
 <div class="toolbox-wrapper">
   <div class="toolbox">
-    <button on:mousedown|preventDefault={ignore} on:click={() => dispatch("bold")} data-test-id="toolbox-bold">B</button>
-    <button on:mousedown|preventDefault={ignore} on:click={() => dispatch("italic")} data-test-id="toolbox-italic">I</button>
-    <button on:mousedown|preventDefault={ignore} on:click={onLink} data-test-id="toolbox-link">Link</button>
+    <button class="bold" on:mousedown|preventDefault={ignore} on:click|preventDefault={() => dispatch("bold")} data-test-id="toolbox-bold">
+      Bold
+    </button>
+    <button class="italic" on:mousedown|preventDefault={ignore} on:click|preventDefault={() => dispatch("italic")} data-test-id="toolbox-italic">
+      Italic
+    </button>
+    <button class="link" on:mousedown|preventDefault={ignore} on:click|preventDefault={onLink} data-test-id="toolbox-link">
+      Link
+    </button>
   </div>
 </div>
